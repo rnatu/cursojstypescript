c1c1c66d34826a7ef983a2dddff0e275e100e998
Renato Xavier <rnatu91@gmail.com>
2020-10-21 15:13:38 -0300

    TypeScript (Tipos básicos) - tipo object
    Aula sobre o tipo object no typescript

---
+++ src/A0004-tipo-object/A0004-tipo-object.ts
@@ -0,0 +1,14 @@
+const objetoA: {
+  readonly chaveA: string;
+  chaveB: string;
+  chaveC?: string;
+  [key: string]: unknown;
+} = {
+  chaveA: 'Valor A',
+  chaveB: 'Valor B',
+};
+
+// objetoA.chaveA = 'Outro valor'; // não pode ser alterada a chave pois é readonly no objeto
+objetoA.chaveC = 'Nova chave';
+objetoA.chaveD = 'Nova chave';
+console.log(objetoA);
