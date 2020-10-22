ae03f192e982c073ea55c8e58e79b0b651eddf7d
Renato Xavier <rnatu91@gmail.com>
2020-10-22 14:30:12 -0300

    TypeScript (Tipos básicos) - tipo undefined e null
    Aula sobre os tipos undefined e null no typescript

---
+++ src/A0007-tipo-null-e-undefined/A0007-tipo-null-e-undefined.ts
@@ -0,0 +1,32 @@
+let x;
+if (x === undefined) x = 2;
+console.log(x * 2);
+
+//% undefined
+export function createPerson(
+  firstname: string,
+  lastname?: string,
+): { firstname: string; lastname?: string } {
+  return {
+    firstname,
+    lastname,
+  };
+}
+
+const person1 = createPerson('Renato');
+person1.lastname === undefined
+  ? console.log('Preenchido seu primeiro nome')
+  : console.log('Preenchido nome completo');
+
+//% null
+export function squareOf(x: any) {
+  if (typeof x === 'number') return x * x;
+  return null;
+}
+
+const result = squareOf('abcdef');
+if (result === null) {
+  console.log('Conta inválida');
+} else {
+  console.log(result * 100);
+}
