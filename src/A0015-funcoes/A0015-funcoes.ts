2b1fbd1e31f9e0d5e7451b9df5c8b60a320f40df
Renato Xavier <rnatu91@gmail.com>
2020-10-27 15:36:53 -0300

    TypeScript (Tipos básicos) - Funções como tipo
    Aula sobre a utilização de funções como tipo em typescript

---
+++ src/A0015-funcoes/A0015-funcoes.ts
@@ -0,0 +1,17 @@
+type mapStringsCallback = (item: string) => string;
+
+export function mapStrings(array: string[], callbackfn: mapStringsCallback): string[] {
+  const newArray: string[] = [];
+
+  for (let i = 0; i < array.length; i++) {
+    const item = array[i];
+    newArray.push(callbackfn(item));
+  }
+
+  return newArray;
+}
+
+const abc = ['a', 'b', 'c'];
+const abcMapped = mapStrings(abc, (item) => item.toLocaleUpperCase());
+
+console.log(abcMapped);
