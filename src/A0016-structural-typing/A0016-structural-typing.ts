1e309aae8f68d55a224655dc25e706b0d6e40d37
Renato Xavier <rnatu91@gmail.com>
2020-10-30 14:58:30 -0300

    TypeScript - Classes e Interfaces (POO) - Classes
    Classes em TypeScript - Moldes
    para objetos

--- src/A0016-structural-typing/A0016-structural-typing.ts
+++ src/A0016-structural-typing/A0016-structural-typing.ts
@@ -2,7 +2,9 @@ type VerifyUserFn = (user: User, sentValue: User) => boolean;
 type User = { username: string; password: string };
 
 const verifyUser: VerifyUserFn = (user, sentValue) => {
-  return user.username === sentValue.username && user.password === sentValue.password;
+  return (
+    user.username === sentValue.username && user.password === sentValue.password
+  );
 };
 
 const sentUser = { username: 'joao', password: '123456', permissions: '' };
--- src/A0019-exercicio/form-control.ts
+++ src/A0019-exercicio/form-control.ts
@@ -20,7 +20,8 @@ form.addEventListener('submit', function (event: Event) {
 
 function checkForEmptyFields(...inputs: HTMLInputElement[]): void {
   inputs.forEach((input) => {
-    if (!input.value) showErrorMessage(input, 'Este campo não pode ficar vazio');
+    if (!input.value)
+      showErrorMessage(input, 'Este campo não pode ficar vazio');
   });
 }
 
@@ -46,13 +47,17 @@ function checkEqualPassword(
 
 function showErrorMessage(input: HTMLInputElement, msg: string): void {
   const formFields = input.parentElement as HTMLDivElement;
-  const errorMessage = formFields.querySelector('.error-message') as HTMLSpanElement;
+  const errorMessage = formFields.querySelector(
+    '.error-message',
+  ) as HTMLSpanElement;
   errorMessage.innerText = msg;
   formFields.classList.add(SHOW_ERROR_MESSAGES);
 }
 
 function shouldSendForm(form: HTMLFormElement): boolean {
   let send = true;
-  form.querySelectorAll('.' + SHOW_ERROR_MESSAGES).forEach(() => (send = false));
+  form
+    .querySelectorAll('.' + SHOW_ERROR_MESSAGES)
+    .forEach(() => (send = false));
   return send;
 }
