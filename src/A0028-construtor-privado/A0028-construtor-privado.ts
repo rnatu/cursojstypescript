d431a7e7a94113cec92bd8695a047fb703bd6478
Renato Xavier <rnatu91@gmail.com>
2020-11-05 13:16:44 -0300

    TypeScript - Classes e Interfaces (POO) - Construtor privado e singleton
    Aula sobre a utilização do construtor privado com padrão de projeto Singleton e Factory method

---
+++ src/A0028-construtor-privado/A0028-construtor-privado.ts
@@ -0,0 +1,37 @@
+//% Singleton - GoF | Factory method - GoF
+export class Database {
+  private static database: Database;
+
+  private constructor(
+    private host: string,
+    private user: string,
+    private password: string,
+  ) {}
+
+  connect(): void {
+    console.log(`Conectado: ${this.host}, ${this.user}, ${this.password}`);
+  }
+
+  static getDatabase(host: string, user: string, password: string): Database {
+    if (Database.database) {
+      console.log('Retornando instância já criada');
+      return Database.database;
+    }
+    console.log('Criando nova instancia');
+    Database.database = new Database(host, user, password);
+    return Database.database;
+  }
+}
+
+const db1 = Database.getDatabase('localhost', 'root', '123456');
+db1.connect();
+
+console.log('');
+
+const db2 = Database.getDatabase('localhost', 'root', '123456');
+db2.connect();
+
+console.log('');
+
+const db3 = Database.getDatabase('localhost', 'root', '123456');
+db3.connect();
