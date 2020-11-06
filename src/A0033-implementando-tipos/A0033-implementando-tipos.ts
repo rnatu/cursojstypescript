d6e7541ea3a312be1317a748b04ad3cdf045f258
Renato Xavier <rnatu91@gmail.com>
2020-11-06 16:55:37 -0300

    TypeScript - Classes e Interfaces (POO) - Implementando o type alias em classes
    Aula sobre a utilização do type alias em classes no typescript

---
+++ src/A0033-implementando-tipos/A0033-implementando-tipos.ts
@@ -0,0 +1,21 @@
+type TipoNome = {
+  nome: string;
+};
+
+type TipoSobrenome = {
+  sobrenome: string;
+};
+
+type TipoNomeCompleto = {
+  nomeCompleto(): string;
+};
+
+export class Pessoa implements TipoNome, TipoSobrenome, TipoNomeCompleto {
+  constructor(public nome: string, public sobrenome: string) {}
+  nomeCompleto(): string {
+    return this.nome + ' ' + this.sobrenome;
+  }
+}
+
+const pessoa = new Pessoa('Renato', 'Silva');
+console.log(pessoa.nomeCompleto());
