9a9d5e8ee2ab17d9f6d9d21b76bbf119c0ffa299
Renato Xavier <rnatu91@gmail.com>
2020-11-24 12:31:50 -0300

    TypeScript - Decorators - Method Decorator
    Aula sobre a utilização de "decoradores" de método no typescript

---
+++ src/A0055-decorator-method/A0055-decorator-method.ts
@@ -0,0 +1,55 @@
+function decorador(
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  classPrototype: any,
+  nomeMetodo: string,
+  descriptor: PropertyDescriptor,
+): PropertyDescriptor | void {
+  console.log(classPrototype);
+  console.log(nomeMetodo);
+  console.log(descriptor);
+  return {
+    // writable: false,
+    value: function (...args: string[]) {
+      return args[0].toUpperCase();
+    },
+  };
+}
+
+export class UmaPessoa {
+  nome: string;
+  sobrenome: string;
+  idade: number;
+
+  constructor(nome: string, sobrenome: string, idade: number) {
+    this.nome = nome;
+    this.sobrenome = sobrenome;
+    this.idade = idade;
+  }
+
+  @decorador
+  metodo(msg: string): string {
+    return `${this.nome} ${this.sobrenome}: ${msg}`;
+  }
+
+  get nomeCompleto(): string {
+    return this.nome + ' ' + this.sobrenome;
+  }
+
+  set nomeCompleto(valor: string) {
+    const palavras = valor.split(/\s+/g);
+    const primeiroNome = palavras.shift();
+
+    if (!primeiroNome) return;
+    this.nome = primeiroNome;
+    this.sobrenome = palavras.join(' ');
+  }
+}
+
+const pessoa = new UmaPessoa('Fernando', 'Brito', 20);
+
+// # alterar o metodo como baixo não irá funcionar se no decorator do metodo for setado como writable: false
+// pessoa.metodo = () => 'EIIIIIIIIIIIIII';
+
+const metodo = pessoa.metodo('Esse é meu nome completo');
+
+console.log(metodo);
